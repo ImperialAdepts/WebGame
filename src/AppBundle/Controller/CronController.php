9ee11ab400c0ceb1f68a56f309eae0716833660e
Troi <web@jurasek.email>
2018-08-24 16:57:50 +0200

    consuption and fertility cycles

--- src/AppBundle/Controller/CronController.php
+++ src/AppBundle/Controller/CronController.php
@@ -3,9 +3,11 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Builder\PlanetBuilder;
+use AppBundle\Descriptor\ResourceDescriptorEnum;
 use AppBundle\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -13,6 +15,9 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class CronController extends Controller
 {
+	const PEOPLE_CONSUMPTION = 1.1;
+	const PEOPLE_STARVATION_RATIO = 0.3;
+	const PEOPLE_BASE_FERTILITY_RATE = 2;
 
 	/**
 	 * @Route("/build-projects", name="cron_build_projects")
@@ -35,14 +40,83 @@ class CronController extends Controller
 
 		$response = "";
 		foreach ($projects as $project) {
-			$response .= "PROJECT at " . $project->getRegion()->getUuid() . ' ' . $project->getBuildingBlueprint()->getDescription()."\n<br>";
+			$response .= "PROJECT at " . $project->getRegion()->getUuid() . ' ' . $project->getBuildingBlueprint()->getDescription() . "\n<br>";
 			if (!$project->getSteplogs()) continue;
 			foreach ($project->getSteplogs() as $log) {
 				$response .= "$log\n<br>";
 			}
 		}
 
-		return new \Symfony\Component\HttpFoundation\Response($response);
+		return new Response($response);
 	}
 
+	/**
+	 * @Route("/maintanance", name="cron_maintanance")
+	 */
+	public function maintananceAction()
+	{
+		$settlements = $this->getDoctrine()->getRepository(Entity\Planet\Settlement::class)->getAll();
+
+		/** @var Entity\Planet\Settlement $settlement */
+		foreach ($settlements as $settlement) {
+			$foodDeposit = $settlement->getResourceDeposit(ResourceDescriptorEnum::SIMPLE_FOOD);
+			$peopleDeposit = $settlement->getResourceDeposit(ResourceDescriptorEnum::PEOPLE);
+			if (!$peopleDeposit) continue;
+
+			$foodNedded = $peopleDeposit ? $peopleDeposit->getAmount() * self::PEOPLE_CONSUMPTION : 0;
+			$foodHave = $foodDeposit ? $foodDeposit->getAmount() : 0;
+
+			if ($foodDeposit) {
+				if ($foodHave > $foodNedded) {
+					$foodDeposit->setAmount($foodHave - $foodNedded);
+				} else {
+					$foodDeposit->setAmount(0);
+					$this->getDoctrine()->getManager()->remove($foodDeposit);
+				}
+				$this->getDoctrine()->getManager()->persist($foodDeposit);
+			}
+
+			if ($foodHave < $foodNedded) {
+				$hungryPeople = ($foodNedded - $foodHave) / self::PEOPLE_CONSUMPTION;
+				$diedPeople = round($hungryPeople * self::PEOPLE_STARVATION_RATIO) + 1;
+				$newPeopleCount = $peopleDeposit->getAmount() - $diedPeople;
+				$peopleDeposit->setAmount($newPeopleCount);
+				if ($newPeopleCount < 1) {
+					$this->getDoctrine()->getManager()->remove($peopleDeposit);
+				}
+			}
+
+			$this->getDoctrine()->getManager()->persist($peopleDeposit);
+		}
+		$this->getDoctrine()->getManager()->flush();
+
+		$response = "";
+
+		return new Response($response);
+	}
+
+	/**
+	 * @Route("/births", name="cron_births")
+	 */
+	public function birthsAction()
+	{
+		$settlements = $this->getDoctrine()->getRepository(Entity\Planet\Settlement::class)->getAll();
+
+		/** @var Entity\Planet\Settlement $settlement */
+		foreach ($settlements as $settlement) {
+			$peopleDeposit = $settlement->getResourceDeposit(ResourceDescriptorEnum::PEOPLE);
+			if (!$peopleDeposit) continue;
+
+			$newborn = round($peopleDeposit->getAmount() * self::PEOPLE_BASE_FERTILITY_RATE / 20) + 1;
+			$newPeopleCount = $peopleDeposit->getAmount() + $newborn;
+			$peopleDeposit->setAmount($newPeopleCount);
+
+			$this->getDoctrine()->getManager()->persist($peopleDeposit);
+		}
+		$this->getDoctrine()->getManager()->flush();
+
+		$response = "";
+
+		return new Response($response);
+	}
 }
