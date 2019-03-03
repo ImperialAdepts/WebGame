625c8da0509a7560faba370e03139e1ba0992eea
Troi <web@jurasek.email>
2019-03-03 20:26:09 +0100

    script na znovuvytvoreni databaze uz nepotrebuje potvrdit smazani

--- recreate_database.sh
+++ recreate_database.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo docker-compose exec php-fpm bin/console doctrine:schema:drop --force
 sudo docker-compose exec php-fpm bin/console doctrine:schema:create
-sudo docker-compose exec php-fpm bin/console doctrine:fixtures:load --env=dev
+sudo docker-compose exec php-fpm bin/console doctrine:fixtures:load --env=dev -q
